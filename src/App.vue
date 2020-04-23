ed66c10c4ddd01c125543317a1700955aee08511
hanbing.hu <hanbing.hu@tellyes.com>
2020-04-23 18:08:24 +0800

    add new page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script>
 	export default {
+		//当uni-app 初始化完成时触发（全局只触发一次）
 		onLaunch: function() {
-			console.log('App Launch')
+			// uni.navigateTo({
+			// 	url:"pages/login/index"
+			// })
 		},
 		onShow: function() {
 			console.log('App Show')
